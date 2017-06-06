9166a87809a6b686e05d14d79382a106a4bcc7bc
raman <raman@raman-Inspiron-N4010>
2017-06-06 20:44:13 +0530

    initial commit for ktos

---
+++ ktos/src/main/java/org/raman/ktos/configuration/KtosApplication.java
@@ -0,0 +1,13 @@
+package org.raman.ktos.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtosApplication.class, args);
+	}
+	
+}
