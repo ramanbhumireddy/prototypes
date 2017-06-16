5a81c75079f41ec728cf924abf4a16024957ad1d
rabhumir <rabhumir@DIN66009480.corp.capgemini.com>
2017-06-16 16:05:10 +0530

    initial commit for jms with examle, before run jms server.

---
+++ gms/src/main/java/org/nsite/main/MQConfiguration.java
@@ -0,0 +1,14 @@
+package org.nsite.main;
+
+import org.apache.activemq.pool.PooledConnectionFactory;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class MQConfiguration {
+
+	
+//	public PooledConnectionFactory getConnectionFactory(){
+//		return new PooledConnectionFactory(activeMQConnectionFactory)
+//	}
+	
+}
